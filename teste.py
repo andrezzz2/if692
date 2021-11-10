96f8ee384f71871dda68696a5d5ae61379f1c73b
DiogoPedro <33842087+DiogoPedro@users.noreply.github.com>
2021-11-09 22:40:55 -0300

    Delete teste.py

--- teste.py
+++
@@ -1,8 +0,0 @@
-cars = ["Ford","ChocolatebananaVolvo", "VolvoChocolate","BMW", "NAda"]
-abc = {"apple", "dasdas", "Volvo"}
-
-
-for x in cars:
-    for y in abc:
-        if (y in x):
-            print(x)
